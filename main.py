9c50350056e73efd756c120a092e8aa4b67a6712
Ludine <ludskieverde@gmail.com>
2021-11-18 17:49:19 +0800

    Removed print() funcs for testing paths

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import os
 
 converter = os.path.dirname(__file__)
-print('\n', converter, '\n')
+main_dir = converter.removesuffix('qt-py')
 
 project = input('project name: ')
-print(project, '\n') 
-
-main_dir = converter.removesuffix('qt-py')
-print(main_dir, '\n') 
+ui = input('ui name: ')
 
 target = f'{main_dir}{project}\\'
-print(target, '\n')
-
-ui = input('ui name: ')
 
 path = f'{converter}\\venv\Scripts\pyuic6.exe -x {target}{ui}.ui'
 
-print(path)
 os.system(path)
 
 input()
